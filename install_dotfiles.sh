e9081e84a020aaea4557cceefbed382a5ed81982
Vasileios Souleles <vsoul@arrikto.com>
2018-09-08 10:51:43 +0300

    Update the install_dotfiles.sh script
    Signed-off-by: Vasileios Souleles <vsoul@arrikto.com>

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
 #
 # Install dotfiles to home directory
-# XXX: Destructive and irreversible
+#
+# XXX: This script is destructive and irreversible
 
 set -e
 
-DIR=~/src/dotfiles
+: "${TARGET_USER:=batzilo}"
+
+: "${SRC_DIR:=/home/$TARGET_USER/src/dotfiles}"
+
+: "${DST_DIR:=/home/$TARGET_USER}"
+
+
+if [ ! -d /home/$TARGET_USER ];
+then
+	echo "/home/$TARGET_USER : No such directory"
+	exit 1
+fi
+
+if [ ! -d $SRC_DIR ];
+then
+	echo "$SRC_DIR : No such directory"
+	exit 1
+fi
+
+if [ ! -d $DST_DIR ];
+then
+	echo "$DST_DIR : No such directory"
+	exit 1
+fi
+
 
 DOTFILES="\
-	bashrc		\
-	gitconfig	\
-	profile		\
-	screenrc	\
-	vimrc		\
-	Xresources	\
+	bashrc            \
+	gitconfig         \
+	profile	          \
+	screenrc          \
+	vimrc             \
+	Xresources        \
+	"
+
+MAYBE_DOTFILES="\
+	local.gitconfig   \
+	arrikto.gitconfig \
 	"
 
 DOTDIRS="\
-	bin	\
-	vim	\
-	xmonad	\
+	bin    \
+	vim    \
+	xmonad \
 	"
 
 for file in $DOTFILES; do
-	echo "Creating a symbolic link of '$DIR/$file' to '~/.$file'"
-	ln -f -s $DIR/$file ~/.$file
+	echo "Creating a symbolic link of '$SRC_DIR/$file' to '$DST_DIR/.$file'"
+	ln -f -s $SRC_DIR/$file $DST_DIR/.$file
+	chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$file
 done
+unset file
+
+for maybe_file in $MAYBE_DOTFILES; do
+	if [ -f "$maybe_file" ];
+	then
+		echo "Creating a symbolic link of '$SRC_DIR/$maybe_file' to '$DST_DIR/.$maybe_file'"
+		ln -f -s $SRC_DIR/$maybe_file $DST_DIR/.$maybe_file
+		chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$maybe_file
+	else
+		echo "Could not find '$SRC_DIR/$maybe_file', continuing..."
+	fi
+done
+unset maybe_file
 
 for dir in $DOTDIRS; do
-	echo "Creating a symbolic link of '$DIR/$dir' to '~/.$dir'"
-	ln -f -s -T $DIR/$dir ~/.$dir
+	echo "Creating a symbolic link of '$SRC_DIR/$dir' to '$DST_DIR/.$dir'"
+	ln -f -s -T $SRC_DIR/$dir $DST_DIR/.$dir
+	chown -R $TARGET_USER:$TARGET_USER $DST_DIR/.$dir
 done
+unset dir
 
 echo "Enjoy!"
