60a0be5af24f0a40da1e81aba62016574604f593
Vasileios Souleles <vasilis.souleles@gmail.com>
2020-08-10 22:19:05 +0300

    Update bins

--- bin/theme_dark.sh
+++ bin/theme_dark.sh
@@ -5,3 +5,6 @@ xrdb        $HOME/src/dotfiles/Xresources
 # xrdb -merge $HOME/src/dotfiles/Xresources/solarized.light
 
 sed -i 's/^colorscheme.*/colorscheme mustang/' $HOME/src/dotfiles/vimrc
+
+# Does not work :(
+# sed -i 's/"userTheme":"light"/"userTheme":"dark"/' $HOME/.config/Slack/storage/slack-settings
--- bin/theme_light.sh
+++ bin/theme_light.sh
@@ -5,3 +5,6 @@ xrdb        $HOME/src/dotfiles/Xresources
 xrdb -merge $HOME/src/dotfiles/Xresources.solarized.light
 
 sed -i 's/^colorscheme.*/colorscheme solarized/' $HOME/src/dotfiles/vimrc
+
+# Does not work :(
+# sed -i 's/"userTheme":"dark"/"userTheme":"light"/' $HOME/.config/Slack/storage/slack-settings
