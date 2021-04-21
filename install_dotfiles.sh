2c00a3a7bc3595c0633f1d9be0b87315de18786e
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-04-21 21:24:06 +0300

    Give install_dotfiles.sh some love

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,50 +1,20 @@
 #!/bin/bash
 #
-# Install dotfiles to home directory
+# Install the dotfiles.
 #
-# XXX: This script is destructive and irreversible
+# Do not simply copy the files from this git repo to $HOME.
+# If then the dotfiles are edited directly,
+# the changes will not be tracked by git.
+#
+# Instead, use symbolic links.
+#
+# Usage:
+# $ ./install_dotfiles.sh [-v] [-i] [-f]
+#
 
 set -e
 
-while :
-do
-        case "$1" in
-                "-v" | "--verbose")
-                        # be verbose
-                        set -x
-                        ;;
-                *)
-                        ;;
-        esac
-        shift || break
-done
-
-: "${TARGET_USER:=batzilo}"
-
-: "${SRC_DIR:=/home/$TARGET_USER/src/dotfiles}"
-
-: "${DST_DIR:=/home/$TARGET_USER}"
-
-
-if [ ! -d /home/$TARGET_USER ];
-then
-	echo "/home/$TARGET_USER : No such directory"
-	exit 1
-fi
-
-if [ ! -d $SRC_DIR ];
-then
-	echo "$SRC_DIR : No such directory"
-	exit 1
-fi
-
-if [ ! -d $DST_DIR ];
-then
-	echo "$DST_DIR : No such directory"
-	exit 1
-fi
-
-
+# Create a symbolic link for those files in $HOME.
 DOTFILES="\
 	bashrc            \
 	gitconfig         \
@@ -54,12 +24,14 @@ DOTFILES="\
 	Xresources        \
 	"
 
+# Create a symbolic link for those files in $HOME, _if_ they exist.
 MAYBE_DOTFILES="\
-	local.gitconfig   \
-	arrikto.gitconfig \
+	local.gitconfig      \
+	arrikto.gitconfig    \
 	blueground.gitconfig \
 	"
 
+# Create a symbolic link for those directories in $HOME.
 DOTDIRS="\
 	bin    \
 	fonts  \
@@ -67,30 +39,70 @@ DOTDIRS="\
 	xmonad \
 	"
 
+: "${TARGET_USER:=$(whoami)}"
+SRC_DIR="$(pwd)"
+DST_DIR="/home/$TARGET_USER"
+ASK_FOR_APPROVAL=""
+USE_FORCE=""
+
+while :
+do
+        case "$1" in
+                "-v" | "--verbose")
+                        # Be verbose.
+                        set -x
+                        ;;
+		"-i" | "--interactive")
+			# Stop and ask for approval.
+			ASK_FOR_APPROVAL="-i"
+			;;
+		"-f" | "--force")
+			# Use (the) force.
+			USE_FORCE="-f"
+			;;
+                *)
+                        ;;
+        esac
+        shift || break
+done
+
+# Check if destination directory exists.
+if [ ! -d $DST_DIR ]; then
+	echo "/home/$TARGET_USER: No such directory exists"
+	exit 1
+fi
+
+# Create symbolic links for the dotfiles.
 for file in $DOTFILES; do
-	echo "Creating a symbolic link of '$SRC_DIR/$file' to '$DST_DIR/.$file'"
-	ln -f -s $SRC_DIR/$file $DST_DIR/.$file
+	echo "Creating a symbolic link for $DST_DIR/.$file"
+	ln -s $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$file $DST_DIR/.$file
 	chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$file
 done
 unset file
 
+# Create symbolic links for the maybe_dotfiles, if any.
 for maybe_file in $MAYBE_DOTFILES; do
-	if [ -f "$SRC_DIR/$maybe_file" ];
-	then
-		echo "Creating a symbolic link of '$SRC_DIR/$maybe_file' to '$DST_DIR/.$maybe_file'"
-		ln -f -s $SRC_DIR/$maybe_file $DST_DIR/.$maybe_file
+	if [ -f $maybe_file ]; then
+		echo "Creating a symbolic link for $DST_DIR/.$maybe_file"
+		ln -s $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$maybe_file $DST_DIR/.$maybe_file
 		chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$maybe_file
-	else
-		echo "Could not find '$SRC_DIR/$maybe_file', continuing..."
 	fi
 done
 unset maybe_file
 
+# Create symbolic links for the dot_directories.
 for dir in $DOTDIRS; do
-	echo "Creating a symbolic link of '$SRC_DIR/$dir' to '$DST_DIR/.$dir'"
-	ln -f -s -T $SRC_DIR/$dir $DST_DIR/.$dir
+	echo "Creating a symbolic link for $DST_DIR/.$dir"
+	ln -s -T $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$dir $DST_DIR/.$dir
 	chown -R $TARGET_USER:$TARGET_USER $DST_DIR/.$dir
 done
 unset dir
+
+# Install black.
+pip install black || pip3 install black
+
+# Install the git submodules.
+git submodule init
+git submodule update
 
 echo "Enjoy!"
