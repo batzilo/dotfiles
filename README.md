e66dedd682b073906b3dde0cf064175ca21eaeff
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-12-30 12:45:43 +0000

    Make install_dotifles.sh idempotent

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-dotfiles
-========
+# dotfiles
 
-My dotfiles
+## Install
+
+```
+$ bash install_dotfiles.sh [-v] [-i] [-f]
+```
+
+Then you may want to create one of the `$MAYBE_DOTFILEs`/`*.gitconfig` files.
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -15,24 +15,24 @@
 set -e
 
 # Create a symbolic link for those files in $HOME.
-DOTFILES="\
-	bashrc            \
-	gitconfig         \
-	profile	          \
-	screenrc          \
-	vimrc             \
-	Xresources        \
+DOTFILES="     \
+	bashrc     \
+	gitconfig  \
+	profile    \
+	screenrc   \
+	vimrc      \
+	Xresources \
 	"
 
 # Create a symbolic link for those files in $HOME, _if_ they exist.
-MAYBE_DOTFILES="\
+MAYBE_DOTFILES="         \
 	local.gitconfig      \
 	arrikto.gitconfig    \
 	blueground.gitconfig \
 	"
 
 # Create a symbolic link for those directories in $HOME.
-DOTDIRS="\
+DOTDIRS="  \
 	bin    \
 	fonts  \
 	vim    \
@@ -47,11 +47,11 @@ USE_FORCE=""
 
 while :
 do
-        case "$1" in
-                "-v" | "--verbose")
-                        # Be verbose.
-                        set -x
-                        ;;
+	case "$1" in
+		"-v" | "--verbose")
+			# Be verbose.
+			set -x
+			;;
 		"-i" | "--interactive")
 			# Stop and ask for approval.
 			ASK_FOR_APPROVAL="-i"
@@ -60,11 +60,16 @@ do
 			# Use (the) force.
 			USE_FORCE="-f"
 			;;
-                *)
-                        ;;
-        esac
-        shift || break
+		*)
+			;;
+	esac
+	shift || break
 done
+
+# Play it safe, approval wins force.
+if [ "$ASK_FOR_APPROVAL" == "-i" ]; then
+	USE_FORCE=""
+fi
 
 # Check if destination directory exists.
 if [ ! -d $DST_DIR ]; then
@@ -72,34 +77,50 @@ if [ ! -d $DST_DIR ]; then
 	exit 1
 fi
 
+create_symblic_link () {
+	SRC_DOTFILE=$SRC_DIR/$file
+	DST_DOTFILE=$DST_DIR/.$file
+	# If destination dotfile exists ...
+	if [ -f $DST_DIR/.$file ]; then
+		SRC_FILE_SHA=$(sha512sum $SRC_DOTFILE | awk '{print $1}')
+		DST_FILE_SHA=$(sha512sum $DST_DOTFILE | awk '{print $1}')
+		# ... and if contents are the same ...
+		if [ "$SRC_FILE_SHA" == "$DST_FILE_SHA" ]; then
+			# ... then skip.
+			return
+		fi
+	fi
+	echo "Creating a symbolic link for $DST_DOTFILE"
+	ln -s $ASK_FOR_APPROVAL $USE_FORCE $SRC_DOTFILE $DST_DOTFILE
+	chown -h $TARGET_USER:$TARGET_USER $DST_DOTFILE
+}
+
 # Create symbolic links for the dotfiles.
 for file in $DOTFILES; do
-	echo "Creating a symbolic link for $DST_DIR/.$file"
-	ln -s $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$file $DST_DIR/.$file
-	chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$file
+	create_symblic_link
 done
 unset file
 
 # Create symbolic links for the maybe_dotfiles, if any.
-for maybe_file in $MAYBE_DOTFILES; do
-	if [ -f $maybe_file ]; then
-		echo "Creating a symbolic link for $DST_DIR/.$maybe_file"
-		ln -s $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$maybe_file $DST_DIR/.$maybe_file
-		chown -h $TARGET_USER:$TARGET_USER $DST_DIR/.$maybe_file
+for file in $MAYBE_DOTFILES; do
+	if [ -f $file ]; then
+		create_symblic_link
 	fi
 done
-unset maybe_file
+unset file
 
 # Create symbolic links for the dot_directories.
 for dir in $DOTDIRS; do
-	echo "Creating a symbolic link for $DST_DIR/.$dir"
-	ln -s -T $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$dir $DST_DIR/.$dir
-	chown -R $TARGET_USER:$TARGET_USER $DST_DIR/.$dir
+	DST_DOTDIR=$DST_DIR/.$dir
+	if [ -d $DST_DOTDIR ]; then
+		# Directory already exists, skip.
+		continue
+	fi
+	echo "Creating a symbolic link for $DST_DOTDIR"
+	ln -s -T $ASK_FOR_APPROVAL $USE_FORCE $SRC_DIR/$dir $DST_DOTDIR
+	chown -R $TARGET_USER:$TARGET_USER $DST_DOTDIR
 done
 unset dir
-
-# Install black.
-pip install black || pip3 install black
 
 # Install the git submodules.
 git submodule init
