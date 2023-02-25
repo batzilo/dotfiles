801a82586971e98483a642d0ae1ef84590f6b379
Vasileios Souleles <vasilis.souleles@gmail.com>
2023-02-25 14:17:36 +0200

    Change the default terminal from xterm to urxvt

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -45,7 +45,7 @@ myModMask            = mod4Mask       -- changes the mod key to "super"
 myFocusedBorderColor = "#ff0000"      -- color of focused border
 myNormalBorderColor  = "#cccccc"      -- color of inactive border
 myBorderWidth        = 1              -- width of border around windows
-myTerminal           = "xterm"        -- which terminal software to use
+myTerminal           = "urxvt"        -- which terminal software to use
 myIMRosterTitle      = "Buddy List"   -- title of roster on IM workspace
                                       -- use "Buddy List" for Pidgin, but
                                       -- "Contact List" for Empathy
